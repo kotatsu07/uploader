de7fea86f704fef050b55b0c2a6b8142aa919e50
kotatsu07 <renshi15sr@gmail.com>
2025-05-12 13:47:16 +0900

    app kaitayo

--- uploader/app.py
+++ uploader/app.py
@@ -1,8 +1,38 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, redirect, url_for, render_template, send_from_directory
 import os
 app = Flask(__name__)
 
 UPLOAD_FOLDER = 'uploads'
 os.makedirs(UPLOAD_FOLDER,exit_ok=True)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
+def allowed_file(filename):
+    return '.'in filename and filename.rsplit('.',1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/',method =['GET','POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+            return redirect(url_for('uploaded_file',filename = filename))
+    return render_template('upload.html')
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],filename)
+
+if __name__ == '__main__':
+    if not os.path.exists(app.config['UPLOAD_FOLLDER']):
+        os.makedirs(app.config['UPLOAD_FOLDER'])
+    app.run(debug=True)
+
+
 
