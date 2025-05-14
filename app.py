94b809734f5f369ee32d79c9bc1720b11700411f
kotatsu07 <renshi15sr@gmail.com>
2025-05-14 10:20:08 +0900

    test

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
 def allowed_file(filename):
     return '.'in filename and filename.rsplit('.',1)[1].lower() in ALLOWED_EXTENSIONS
-
-@app.route('/',method =['GET','POST'])
+    
+@app.route('/',methods =['GET','POST'])
 def upload_file():
     if request.method == 'POST':
         if 'file' not in request.files:
@@ -30,7 +30,7 @@ def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'],filename)
 
 if __name__ == '__main__':
-    if not os.path.exists(app.config['UPLOAD_FOLLDER']):
+    if not os.path.exists(app.config['UPLOAD_FOLDER']):
         os.makedirs(app.config['UPLOAD_FOLDER'])
     app.run(debug=True)
 
